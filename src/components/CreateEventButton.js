27aa5c8dfffdefe20d7d80ab420316482d9606c3
PrakarshKamal <prakarshkamal@Prakarshs-MBP.cgocable.net>
2022-12-07 20:53:55 -0500

    fixing small calendar css

--- src/components/CreateEventButton.js
+++ src/components/CreateEventButton.js
@@ -1,10 +1,16 @@
 import React from "react";
 import plus from "../assets/plus.svg";
+
 export default function CreateEventButton() {
   return (
-    <button className="border p-2 rounded-full flex items-center shadow-md hover:shadow-2xl">
-      <img src={plus} alt="create_event" className="w-7 h-7" />
-      <span className="pl-3 pr-7"> Create</span>
-    </button>
+    <p>
+      <button
+        id="createEventButton"
+        className="border rounded-full flex items-center shadow-md hover:shadow-2xl"
+      >
+        <img src={plus} alt="create_event" className="w-7 h-7" />
+        <span className="pl-3 pr-7"> Create</span>
+      </button>
+    </p>
   );
 }
--- src/components/SmallCalendar.js
+++ src/components/SmallCalendar.js
@@ -1,5 +1,32 @@
-import React from "react";
+import dayjs from "dayjs";
+import React, { useEffect, useState } from "react";
+import { getMonth } from "../utils";
 
 export default function SmallCalendar() {
-  return <div>SmallCalendar</div>;
+  const [currentMonthIdx, setCurrentMonthIdx] = useState(dayjs().month());
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+
+  useEffect(() => {
+    setCurrentMonth(getMonth(currentMonthIdx));
+  }, [currentMonthIdx]);
+
+  return (
+    <div id="smallCalendar">
+      <header className="flex justify-between">
+        <p className="text-gray-600">
+          {dayjs(new Date(dayjs().year(), currentMonthIdx)).format("MMMM YYYY")}
+        </p>
+        <button>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_left
+          </span>
+        </button>
+        <button>
+          <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+            chevron_right
+          </span>
+        </button>
+      </header>
+    </div>
+  );
 }
