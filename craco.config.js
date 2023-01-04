a9b0c2264bb0796c2685c298861fb91818a8f361
Prakarsh Kamal <pkamal@venacorp.com>
2023-01-04 16:03:00 -0500

    update

--- craco.config.js
+++ craco.config.js
@@ -5,3 +5,19 @@
 //     },
 //   },
 // };
+
+const path = require('path')
+
+module.exports = {
+  entry: './js/app.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{ test: /\.ts$/, use: 'ts-loader' }],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+}
